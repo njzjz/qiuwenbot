c6bc7c2029de443576435d1623c8912226b0cbc1
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-02-09 18:01:04 -0500

    improving replacing flags

--- qiuwenbot/replaceroc.py
+++ qiuwenbot/replaceroc.py
@@ -20,6 +20,7 @@ from tqdm import tqdm
 from tqdm.contrib.logging import logging_redirect_tqdm
 
 from .bot import login, get_page
+from .utils import archieve_page
 
 
 def logging(site: Site, user: str, title: str, n: int) -> None:
@@ -37,6 +38,8 @@ def logging(site: Site, user: str, title: str, n: int) -> None:
         number of removed references
     """
     page = get_page("User:%s/replaced_roc_flags_log" % user, site)
+    if len(page.text.split("\n")) > 2000:
+        page = archieve_page(page, site)
     page.text += "\n# [[%s]] - replaced %d flags - ~~~~~" % (title, n)
     page.save("[[User:Njzjzbot/task1|记录替换旗帜的条目]]")
 
@@ -55,14 +58,17 @@ def clean_roc(site: Site, user: str):
 
     # compiled res
     re_birth = re.compile(r'Category:(\d+)年出生')
-    re_death = re.compile(r'Category:(\d+)年死亡')
+    re_death = re.compile(r'Category:(\d+)年逝世')
     re_found = re.compile(r'Category:(\d+)年(.*)(设立|建立|成立|创建)(.*)')
 
     re_roc = re.compile(r'({{[\s]*ROC[\s]*}})')
     re_nationality = re.compile(r'\|[\s]*(國籍|国籍|nationality)[\s]*=[\s]*({{[\s]*ROC[\s]*}})')
     re_death_place = re.compile(r'\|[\s]*(逝世地點|逝世地点|death_place)[\s]*=[\s]*({{[\s]*ROC[\s]*}})')
 
-    replaced_chn = "{{CHN}}<!-- replaced_flag 0 by %s -->" % user
+    comment = "<!-- replaced_flag 0 by %s -->" % user
+    replaced_chn = r"{{CHN}}" + comment
+    replaced_chn_nationality = r"|\1={{PRC-TWN}}" + comment
+    replaced_death_place = r"|\1={{CHN}}" + comment
 
     with logging_redirect_tqdm():
         n_replaced = tqdm(position=2, desc="Replaced flags")
@@ -71,6 +77,8 @@ def clean_roc(site: Site, user: str):
             reason = None
             replace_all = False
             replace_death = False
+            replace_nationality = False
+            yy = []
             for cat in page.categories():
                 m = re_birth.match(cat.title())
                 if m:
@@ -78,6 +86,7 @@ def clean_roc(site: Site, user: str):
                     if int(y) > 1949:
                         reason = "新中国成立后出生的人物"
                         replace_all = True
+                        replace_nationality = True
                         break
                 m = re_death.match(cat.title())
                 if m or cat.title() == '在世人物':
@@ -85,30 +94,36 @@ def clean_roc(site: Site, user: str):
                     if int(y) > 1949:
                         reason = "在世人物或新中国成立后逝世的人物"
                         replace_death = True
+                        replace_nationality = False # need a new rule to clean
                         break
                 m = re_found.match(cat.title())
                 if m:
                     y = m.group(1)
-                    if int(y) > 1949:
-                        reason = "新中国成立后出现的事物"
-                        replace_all = True
-                        break
+                    yy.append(int(y))
+            if len(yy) and min(yy) > 1949:
+                reason = "新中国成立后出现的事物"
+                replace_all = True
             if reason is not None:
                 n = 0
+                if replace_nationality:
+                    # nationality
+                    try:
+                        page.text, n1 = re_nationality.subn(replaced_chn_nationality, page.text)
+                    except:
+                        continue
+                    n += n1
+                if replace_death:
+                    try:
+                        page.text, n1 = re_death_place.subn(replaced_death_place, page.text)
+                    except:
+                        continue
+                    n += n1
                 if replace_all:
                     try:
                         page.text, n1 = re_roc.subn(replaced_chn, page.text)
                     except:
                         continue
                     n += n1
-                elif replace_death:
-                    # nationality
-                    try:
-                        page.text, n1 = re_nationality.subn(replaced_chn, page.text)
-                        page.text, n2 = re_death_place.subn(replaced_chn, page.text)
-                    except:
-                        continue
-                    n += n1 + n2
                 if n:
                     try:
                         page.save(("[[User:Njzjzbot/task1|替换%d个非法旗帜]] - " % n) + reason)
