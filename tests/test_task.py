b6be88d982bf358a398b94e8499b1656ad9acc41
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-07-29 00:55:04 -0400

    feat: scan a single page
    Signed-off-by: Jinzhe Zeng <jinzhe.zeng@rutgers.edu>

---
+++ tests/test_task.py
@@ -0,0 +1,61 @@
+import os
+import pytest
+
+import pywikibot
+from pywikibot import Page
+
+from qiuwenbot.qwlogger import qwlogger
+from qiuwenbot.task import Task
+
+
+class UnitTestTask(Task):
+    """A task for unit test.
+
+    Parameters
+    ----------
+    user : str
+        Username.
+    password : str
+        Password.
+    pages : str
+        Pages to operate.
+    """
+
+    def __init__(
+        self,
+        user: str,
+        password: str,
+        pages: dict,
+    ):
+        """Initialize."""
+        super().__init__(
+            user,
+            password,
+            pages,
+            r"User:%s/unittest" % user,
+            "单元测试",
+        )
+
+    def do(self, page: Page) -> bool:
+        """Do the task."""
+        page.text = "单元测试，时间戳：%s" % pywikibot.Timestamp.now().isoformat()
+        try:
+            page.save("njzjz/qiuwenbot: 单元测试")
+        except (pywikibot.exceptions.Error,):
+            qwlogger.exception("Failed to save page %s" % page.title())
+            return False
+        return True
+
+
+@pytest.skipIf(os.environ.get("QIUWENBOT_TEST_USER") is None, "No test user.")
+@pytest.skipIf(os.environ.get("QIUWENBOT_TEST_PASSWORD") is None, "No test password.")
+def test_task():
+    """Test task."""
+    user = os.environ.get("QIUWENBOT_TEST_USER")
+    password = os.environ.get("QIUWENBOT_TEST_PASSWORD")
+    task = UnitTestTask(user=user, password=password, pages={
+        "type": "page",
+        "page": "User:%s/unittest" % user,
+        }
+    )
+    task.submit()
