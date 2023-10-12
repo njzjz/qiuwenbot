f5c89854080e227b7f018b9d77b3388342f16675
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2023-10-11 20:56:24 -0400

    docs: remove sphinx_rtd_theme from extension (#55)

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ author = "Jinzhe Zeng"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx_rtd_theme",
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
     "sphinx.ext.intersphinx",
